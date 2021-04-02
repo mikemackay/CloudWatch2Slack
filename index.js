e7069fca0e68843abab0e848b4b37c553a3d7845
Mike Mackay <coding@mikemackay.co.uk>
2021-04-02 14:57:09 +0100

    Updated to use Slack's correct Block formatting system
    No longer uses "attachments" to format text, as it is being deprecated.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 
-// Load in our libs
-const axios = require('axios');
+// Load in our libraries
 const aws = require('aws-sdk');
-let ec2 = new aws.EC2();
+const axios = require('axios');
 
-// To get the Slack hook url, go into Slack admin and create a new "Incoming Webhook" integration
+// To get your Slack hook URL, go into Slack admin and create a new "Incoming Webhook" integration
 const slackUrl = process.env.SLACK_POST_URL;
 
 exports.handler = async (event) => {
@@ -13,33 +12,50 @@ exports.handler = async (event) => {
     // Grab the inbound notification message
     let snsMessage = JSON.parse(event.Records[0].Sns.Message);
     
-    // Build our Slack POST object, with styling attachment(s)
-    var slackMessageData = {
-        "text": event.Records[0].Sns.Subject + "\n" + snsMessage.NewStateReason,
-        "attachments": [
+    // Build our Slack POST object, with styling blocks
+    var slackMessagePayload = {
+        "text": event.Records[0].Sns.Subject,
+        "blocks": [
             {
-                "fallback": "CloudWatch Alert System",
-                "color": (snsMessage.NewStateValue == "ALARM")? "danger" : "good",
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*" + event.Records[0].Sns.Subject + "*"
+                }
+            },
+            {
+                "type": "section",
                 "fields": []
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "plain_text",
+                    "text": snsMessage.NewStateReason,
+                    "emoji": false
+                }
             }
         ]
     };
     
-    // Get the Dimensions attribute to parse any associated Metadata
+    // Get the Dimensions attribute to parse any associated Metadata, then loop through and work on each dimension (where required)
     let alertDimensions = snsMessage.Trigger.Dimensions;
-    
-    // Loop through and work on each dimension (where required)
     for (var i = 0, len = alertDimensions.length; i < len; i++) {
         if(alertDimensions[i].name == 'InstanceId') {
             let ec2InstanceTags = await getEc2InstanceTags(alertDimensions[i].value);
             ec2InstanceTags.Tags.forEach(function(instanceTag) {
-                slackMessageData.attachments[0].fields.push({"title": instanceTag.Key, "value": instanceTag.Value, "short": true});
+                slackMessagePayload.blocks[1].fields.push(
+                    {
+                        "type": "mrkdwn",
+                        "text": "*" + instanceTag.Key + "*\n" + instanceTag.Value
+                    }
+                );
             });
         }
     }
 
-    // Post to Slack
-    await axios.post(slackUrl, slackMessageData, {
+    // Post our complete message to Slack
+    await axios.post(slackUrl, slackMessagePayload, {
         headers: {
             'Content-Type': 'application/json'
         }
@@ -49,6 +65,7 @@ exports.handler = async (event) => {
 };
 
 function getEc2InstanceTags(instanceId) {
+    let ec2 = new aws.EC2();
     let params = {Filters: [{ Name:"resource-id", Values:[ instanceId ]}]};
     return new Promise((resolve, reject) => {
         let instanceTags = ec2.describeTags(params).promise();
